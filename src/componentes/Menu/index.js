b029151e2d660090986bbbc2bb1ebdf4000574b3
dgusfr <diegogustavo.fr@gmail.com>
2024-12-10 08:42:48 -0300

    update

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,11 +1,14 @@
 import MenuLink from "../MenuLink";
 import styles from "./Menu.module.css";
+import logo from "../../assets/conquisti.png";
 
 export default function Menu() {
   return (
     <header>
       <nav className={styles.navegacao}>
-        <MenuLink to="/">CyberVerse</MenuLink>
+        <MenuLink to="/">
+          <img src={logo} alt="ConquisTi Logo" className={styles.logo} />
+        </MenuLink>
       </nav>
     </header>
   );
